998f8218154971d71b62d8f0fff12c13ee9d54a5
Andi Exer <andiexer@CODEBOOK.local>
2024-01-11 09:31:45 +0100

    init 2024 with new config and fixed k9s plugins and settings

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,20 +1,15 @@
 #!/bin/sh
 
-# work
-alias dg-login='kinit andreas.exer@INTRANET.DIGITEC' 
-alias ktest="kubectl config use-context test"
-alias kprod="kubectl config use-context prod"
-alias k9="k9s -n dg-sales"
+alias dg-login='kinit andreas.exer@INTRANET.DIGITEC'
+alias ktest='kubectl config use-context $(kubectl config get-contexts -o name | grep k8s-tes)'
+alias kprod='kubectl config use-context $(kubectl config get-contexts -o name | grep k8s-pro)'
+alias k9='k9s -n dg-sales'
 
-# ls
-alias l='exa -l --icons --git --no-user'
-alias ll='exa -l --icons --git -a'
-alias lt='exa --tree --level=2 --long --icons --git'
+alias l='eza -l --icons --git --no-user'
+alias ll='eza -l --icons --git -a'
+alias lt='eza --tree --level=2 --long --icons --git'
 
 alias cat='bat'
-alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
 
 alias df='df -h'
 alias free='free -m'
-
-alias editzsh='nvim ~/.zshrc'
